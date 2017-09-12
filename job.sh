ede8304061b38a04073acd02e1a56bdad4f3b12e
roryh2703 <scrh@comp-pc3240.leeds.ac.uk>
2017-09-12 10:23:08 +0100

    Small changes to purging files

--- job.sh
+++ job.sh
@@ -8,7 +8,7 @@
 #$ -M scrh@leeds.ac.uk
 
 #    Set time 
-#$ -l h_rt=10:00:00       # time needed
+#$ -l h_rt=48:00:00       # time needed
 
 #    How many nodes, how many processors per node (16 cores per 1 node)
 #$ -l nodes=3,ppn=24 
